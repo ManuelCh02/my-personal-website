83f680e8ba6623c96fd56d1efbe2c22061d1374c
Manuel Chingay <chingayloez4@gmail.com>
2026-07-02 22:24:33 -0500

    Adding styles for education section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,16 +1,20 @@
+import style from "../styles/Education.module.css"
+
 export function Education () {
     return (
-        <section>
-            <div>
-                <h3>EDUCATION</h3>
-                <div>
-                    <span>2020 - PRESENT</span>
-                    <h4>B.Sc. in Computer Science</h4>
-                    <span>University of Lisbon</span>
-                    <p>Graduated with honors. Focus on distributed systems and HCI.</p>
+        <section className={style.educationContainer}>
+            <div className={style.educationCollege}>
+                <h3>E D U C A T I O N</h3>
+                <div className={style.educationCard}>
+                    <span className={style.educationDate}>2020 - PRESENT</span>
+                    <h4>Tecnología en Desarrollo de Software</h4>
+                    <span className={style.educationCollegeName}>Universidad Nacional abierta y a distancia</span>
+                    <p>Cursando la carrera</p>
                 </div>
             </div>
-            <div>
+            {
+            // Will be there soon ;)
+            /* <div>
                 <h3>CERTIFICATIONS</h3>
                 <ul>
                     <li>
@@ -20,7 +24,7 @@ export function Education () {
                         Google UX Design Professional Certificate (2021)
                     </li>
                 </ul>
-            </div>
+            </div> */}
         </section>
     )
 } 
