7904485f645003b4e21f843508d70ee96fdf472f
Manuel Chingay <chingayloez4@gmail.com>
2026-07-02 21:59:59 -0500

    Adding styles for Project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,11 @@
+import style from "../styles/Project.module.css"
+
 export function ProjectCard () {
     return (
         <div>
             <h4>Openmetrics</h4>
             <p>Open-source, self-hostable analytics dashboard with privacy-first event tracking.</p>
-            <ul>
+            <ul className={style.techContainer}>
                 <li>TypeScript</li>
                 <li>React</li>
                 <li>Next.js</li>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,10 @@
 import { ProjectCard } from "./ProjectCard.jsx"
+import style from "../styles/Project.module.css"
 
 export function Projects () {
     return (
-        <section>
-            <h3>PROJECTS</h3>
+        <section className={style.projectSectionContainer}>
+            <h3 className={style.projectTitle}>P R O J E C T S</h3>
             <ProjectCard />
         </section>
     )
