68e0c436537bd8b85b34f1f2cc0e127a630f2710
Manuel Chingay <chingayloez4@gmail.com>
2026-07-04 00:21:20 -0500

    Adding styles for tablet breakpoint--Experience section

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,26 +1,28 @@
 export function ExperienceCard ({experienceData}) {
     return (
-        <div>
+        <div className="experienceCardContainer">
             {
                 experienceData.map(data => {
                     return (
                         <div className="experience-data">
-                            <span>{data.startDate} - {data.endDate}</span>
-                            <div>
-                                <h4>{data.title}</h4>
-                                <span className="experience-type">{data.type}</span>
-                            </div>
-                            <p>{data.description}</p>
-                            <ul className="goal-container">
-                                {
-                                    data.goals.map(goal => <li>{goal}</li>)
-                                }
-                            </ul>             
-                            <ul className="tech-container">
-                                {
-                                    data.technologies.map(tech => <li>{tech}</li>)
-                                }
-                            </ul>
+                            <span className="experience-data-date">{data.startDate} - {data.endDate}</span>
+                            <div className="experience-data-infoContainer">
+                                <div>
+                                    <h4>{data.title}</h4>
+                                    <span className="experience-type">{data.type}</span>
+                                </div>
+                                <p>{data.description}</p>
+                                <ul className="goal-container">
+                                    {
+                                        data.goals.map(goal => <li>{goal}</li>)
+                                    }
+                                </ul>             
+                                <ul className="tech-container">
+                                    {
+                                        data.technologies.map(tech => <li>{tech}</li>)
+                                    }
+                                </ul>
+                            </div>              
                         </div>
                     )
                 })
