6a7254947a2d04ef5fc44619ed73c963e885931e
Manuel Chingay <chingayloez4@gmail.com>
2026-07-11 02:31:41 -0500

    refactor/pageNavigation

--- src/components/PageNavigation.jsx
+++ src/components/PageNavigation.jsx
@@ -1,4 +1,4 @@
-import "../styles/PageNavigation.css"
+import style from "../styles/PageNavigation.module.css"
 
 const navLinks = [
   {
@@ -30,12 +30,12 @@ const navLinks = [
 
 export function PageNavigation () {
     return (
-        <nav className="pageNavigationContainer">
-          <ul className="pageNavigationList">
+        <nav className={style.container}>
+          <ul className={style.list}>
             {navLinks.map(link =>
               <li key={link.id}>
-                <a href={link.href} className="link-nav">
-                  <span className="transition"></span>
+                <a href={link.href} className={style.link}>
+                  <span className={style.transition}></span>
                   <span>{link.label}</span>
                 </a>
               </li>
